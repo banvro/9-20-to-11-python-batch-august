593068ec36bc0244b1cd03fef95445ed2bdf417b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-23 22:25:37 -0700

    Create quesrion1.py

---
+++ newbatch-august/quesrion1.py
@@ -0,0 +1,43 @@
+ab = [12, 90, 71, 89, 200, 40, 9, 21, 71]
+# reduce(function,iteration)
+# ab(lambda x,y:x+y,ab)
+# print(ab)
+
+
+# a = 10
+# from functools import reduce
+# b = reduce(lambda x,y:x+y,ab)
+# print(b)
+# a = filter(lambda x : x%3 != 0,ab)
+# print(list(a))
+
+
+# a=[2, 4, 8, 16, 32, 64]
+# total=0
+
+# for i in a:
+#     total=total*i
+#     print(total)
+
+
+
+
+# a = 2
+# for i in range(0,6):
+#     print(i*2)
+# 2, 4, 8, 16, 32, 64
+
+
+
+# questions :
+
+
+
+
+
+
+# print series : 
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+# 2, 4, 8, 16, 32, 64
+
+# print 27 in output, the condation is you only use integer 1 time in code that is 10.
