42b80ec9803e2aca5398e843ee6e9bee4900c874
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-27 23:12:03 -0700

    Create recursion.py

---
+++ newbatch-august/recursion.py
@@ -0,0 +1,56 @@
+# Recursion : when a function call itelf
+
+
+# factotial 
+
+# def fatoril(n):
+#     print(n)
+#     if n <= 0:
+#         return 1
+#     else:
+#         return n * fatoril(n-1)
+#                 # 5 * fatoril(5 - 1)
+#                 # 5 * 4
+
+# z = fatoril(5)
+# print(z)
+
+
+# def fabni(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fabni(n-1) + fabni(n-2)
+
+# z = fabni(4)
+
+# print(z)
+
+
+# 0, 1, 2, 3, 5, 8, 13
+
+
+
+# def countdown(n):
+#     if n <= 0:
+#         print("goooo")
+#     else:
+#         print(n)
+#         countdown(n-1)
+        
+# countdown(6)
+
+
+# c = 0
+
+# def sumthois(a, b):
+#     c = a + b
+#     return c
+    
+# k = sumthois(12, 10)
+
+# z = 10 + k
+
+# print(z)
