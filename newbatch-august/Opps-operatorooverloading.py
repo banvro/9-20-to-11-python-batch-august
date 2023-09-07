8cf4d2465598168b0929d8b4c0822a19307995cc
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-06 22:28:58 -0700

    Update Opps-operatorooverloading.py

--- newbatch-august/Opps-operatorooverloading.py
+++ newbatch-august/Opps-operatorooverloading.py
@@ -37,22 +37,71 @@
 
         
     
-class cls1:
-    def sumthis(self, a = None, b = None, c = None):
-        if a != None and b != None and c != None:
-            zx = a + b + c
-            print(zx)
+# class cls1:
+#     def sumthis(self, a = None, b = None, c = None):
+#         if a != None and b != None and c != None:
+#             zx = a + b + c
+#             print(zx)
         
-        elif a != None and b != None:
-            zx = a + b
-            print(zx)
+#         elif a != None and b != None:
+#             zx = a + b
+#             print(zx)
         
-        elif a != None:
-            zx = a
-            print(zx)
+#         elif a != None:
+#             zx = a
+#             print(zx)
         
-        else:
-            print("Enter something..")
+#         else:
+#             print("Enter something..")
     
-obj = cls1()
-obj.sumthis(12, 100, 1)
+# obj = cls1()
+# obj.sumthis(12, 100, 1)
+   
+   
+# method overrriding : 
+
+# age = 18
+
+
+# age = 90
+
+
+# print(age)
+
+# class cls1:
+#     def showme(self):
+#         print("i am from class 1")
+    
+#     def ok(self):
+#         print("i am ok//....")
+
+# class cls2(cls1):
+#     def no(self):
+#         print("noooooooooo")
+    
+#     def showme(self):
+#         print("i am from class 2...")
+    
+# obj = cls2()   
+
+# obj.showme()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+   
+   
