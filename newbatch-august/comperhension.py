d8f3f60c96c149396c76114c398363f341075dfa
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-10 21:47:44 -0700

    Create comperhension.py

---
+++ newbatch-august/comperhension.py
@@ -0,0 +1,35 @@
+# # 1) Comperhension : 
+# u = int(input("Enter how many numbers you want to enter : "))
+# nums = [(int(input("Enter number : "))) for i in range(u)]
+
+# print(nums)
+
+
+
+# comperhension :
+
+# 1) List comhension
+# 2) set comperhension
+# 3) dict comperhesion
+
+# [expression itreater condation]
+
+# a = [i  for i in range(1, 10) if i % 2 == 0 and i != 6]
+
+# print(a)
+
+# 2) Set comperhension :
+
+# {key expresion : value expression itreater condation }
+
+# a = {i * 2 : i  + 10  for i in range(1, 5)}
+
+# print(a)
+
+a = ["name", "age", "number"]
+
+b = ("moris", 20, 91823691)
+
+# output : 
+
+{"name" : "moris", "age" : 20, "number" : 91823691}
