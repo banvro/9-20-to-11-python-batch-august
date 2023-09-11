3b4562da88f7d6f1089471a730defd868c58d46b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-10 22:38:11 -0700

    Update comperhension.py

--- newbatch-august/comperhension.py
+++ newbatch-august/comperhension.py
@@ -33,3 +33,70 @@ b = ("moris", 20, 91823691)
 # output : 
 
 {"name" : "moris", "age" : 20, "number" : 91823691}
+
+
+
+
+
+
+
+
+
+# # 1) Comperhension : 
+# u = int(input("Enter how many numbers you want to enter : "))
+# nums = [(int(input("Enter number : "))) for i in range(u)]
+
+# print(nums)
+
+
+
+# comperhension :
+
+# 1) List comhension
+# 2) set comperhension
+# 3) dict comperhesion
+
+# [expression itreater condation]
+
+# a = [i  for i in range(1, 10) if i % 2 == 0 and i != 6]
+
+# print(a)
+
+# 2) Set comperhension :
+
+# {key expresion : value expression itreater condation }
+
+# a = {i * 2 : i  + 10  for i in range(1, 5)}
+
+# print(a)
+
+# a = ["name", "age", "number"]
+
+# b = ("moris", 20, 91823691)
+
+# # output : 
+
+# # {"name" : "moris", "age" : 20, "number" : 91823691}
+
+# ac = {a[i] : b [i] for i in range(len(a))}
+
+# print(ac)
+
+a = "i am a python devloper, who are you"
+
+b = ["a", "e", "i", "o", "u"]
+
+
+# for i in a:
+#     if i in b:
+#         continue
+#     print(i)
+
+zx = [i for i in a if i not in b]
+
+print(zx)
+
+
+
+
+
