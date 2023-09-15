a66be9349fe3b53392a5598849868f65c5fdf5d3
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-14 22:02:05 -0700

    Create exception-heandling.py

---
+++ newbatch-august/exception-heandling.py
@@ -0,0 +1,111 @@
+# types of error 
+    # 1) compile time errror
+    # 2) Logical Error 
+    # 3) Run time error
+
+
+# age = 
+
+
+# if age == 18
+#     print("you are 18")
+
+# a = 10
+# b = 0
+
+# c = a / b
+
+# print(c)
+
+
+
+
+
+
+# age = int(input("ENter your age : "))
+
+
+# print("user age sis :", age)
+
+
+# a = 10
+# b = 0
+
+# c = a / b
+ 
+# print(c)
+
+# print("i am runnning...")
+
+# print("hello")
+
+# print("done...")
+
+
+
+# try:
+#     # doutable code 
+
+# except Exception:
+#     # here handel the errors
+
+# else:
+
+
+# finally:
+
+
+# a = 10
+# b = 0
+
+# try:
+#     c = a / b
+
+# except Exception:
+#     print("this is a eerror")
+
+
+
+
+a = 10
+b = 6
+
+try:
+    c = a / b
+    age = int(input("Enerter Age : "))
+
+except Exception:
+    print("this is a eerror")
+
+else:
+    print("the output is : " , c)
+    print("User age is : ", age)
+finally:
+    print("i am done")
+    
+print("running........")
+print("DOne")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
