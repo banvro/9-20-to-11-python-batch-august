d37231e83888a7eccca8fa47cc317db8153c2ed7
Naresh Kumar <banvro07@gmail.com>
2023-09-26 22:11:00 -0700

    save data in database

--- newbatch-august/tkinter-2.py
+++ newbatch-august/tkinter-2.py
@@ -1,11 +1,26 @@
 import tkinter as tk
+import mysql.connector
+
+
+conn = mysql.connector.connect(host = "localhost", username = "root", password="1234", database = "morning")
+
+couser = conn.cursor()
+
+# couser.execute("ajsdashdvk"
 
 def savedata():
     en1 = entr1.get()
     en2 = entr2.get()
     en3 = entr3.get()
-    print(en1, en2, en3)
+    
+    couser.execute(f"insert into savedata values('{en1}', '{en2}', '{en3}')")
+    conn.commit()
 
+    entr1.delete(3, tk.END)
+    entr2.delete(0, tk.END)
+    entr3.delete(0, tk.END)
+
+    labl7.config(text="Data save sucessfully...!")
     # cusers.excute(f"inser into tablename values('{en1}', {en2}, 'xyz@gmail.com')")
 
 app = tk.Tk()
@@ -19,7 +34,7 @@ labl3 = tk.Label(app, text = "Email", font=("robort", 20, "bold"))
 labl4 = tk.Label(app, text = ":", font=("robort", 20, "bold"))
 labl5 = tk.Label(app, text = ":", font=("robort", 20, "bold"))
 labl6 = tk.Label(app, text = ":", font=("robort", 20, "bold"))
-
+labl7 = tk.Label(app, text="", font=("robort", 15, "bold"))
 
 entr1 = tk.Entry(app, font=("robort", 20, "italic"))
 entr2 = tk.Entry(app, font=("robort", 20, "italic"))
@@ -35,6 +50,9 @@ entr1.grid(row=0, column=2, pady=10)
 entr2.grid(row=1, column=2, pady=10)
 entr3.grid(row=2, column=2, pady=10)
 btn.grid(row=3, column=1, pady=10)
+
+labl7.grid(row=4, column=1)
+
 
 labl4.grid(row=0, column=1)
 labl5.grid(row=1, column=1)
