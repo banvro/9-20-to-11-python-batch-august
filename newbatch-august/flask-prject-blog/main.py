2e66733e4520717287a7c98775a6994d8634e996
Naresh Kumar <banvro07@gmail.com>
2023-10-24 21:17:30 -0700

    flask project

---
+++ newbatch-august/flask-prject-blog/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+web = Flask(__name__)
+
+@web.route("/")
+def home():
+    return render_template("home.html")
+
+@web.route("/about")
+def about():
+    return render_template("about.html")
+
+@web.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@web.route("/services")
+def services():
+    return render_template("services.html")
+
+
+if __name__ == "__main__":
+    web.run(debug = True)
