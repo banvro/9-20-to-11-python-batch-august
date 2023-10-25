d0f14041745c006df30864bcec6e6ff4b07c4915
Naresh Kumar <banvro07@gmail.com>
2023-10-24 21:59:56 -0700

    get data in python main file

--- newbatch-august/flask-prject-blog/main.py
+++ newbatch-august/flask-prject-blog/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 web = Flask(__name__)
 
@@ -18,6 +18,16 @@ def contact():
 def services():
     return render_template("services.html")
 
+@web.route("/savedata", methods = ["POST"])
+def savedata():
+    if request.method == "POST":
+        fname = request.form.get("fullname")
+        # x = request.form["fullname"]
+        email = request.form.get("email")
+        p_number = request.form.get("number")
+        message = request.form.get("msg")
+
+    return f"nmae = {fname} , email = {email} phone number = {p_number} and message = {message}"
 
 if __name__ == "__main__":
     web.run(debug = True)
