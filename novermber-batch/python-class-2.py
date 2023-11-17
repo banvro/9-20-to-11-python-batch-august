97d10d1608dfb6681dd7e7bd076116f56cc7d076
Naresh Kumar <banvro07@gmail.com>
2023-11-16 20:55:22 -0800

    class 2

---
+++ novermber-batch/python-class-2.py
@@ -0,0 +1,183 @@
+# print("hello ")
+
+
+# print "hlo"
+
+# range()
+
+# xrange()
+
+
+# variables : 
+    # 1) Variable never be start from any integer or any spacial symol.
+    # 2) always strat from cheracter or undersocer.
+    # 3) varibale never contain spaces
+    # 4) never a keywords.
+
+# int 
+# print
+
+
+
+# age = 20
+
+# 1a = 20
+
+
+# age12 = 30
+
+# _my_name = "moris"
+
+
+
+
+
+
+# Data types in python:
+# 1) Interger : 1, 2, 3, -12, 23, 1, 2
+# 2) Float : 23.0, 1.0, 34, 4.8
+# 3) String : "10"
+#     "this is a sting"
+#     'this is also a sting'
+#     """this is also a sting"""
+# 4) Boolean : True , False
+# 5) Complex : a + bj
+#             10 + 20j
+# 6) List   [12, 20, 10, 2]
+# 7) Tuple (90, 4, 5)
+# 8) Set  : {10, 20, 89, 01}
+# 9) Dictionry : {key : value}
+
+
+
+# a = 10
+
+
+# age = 10
+
+
+# if (10 == 10){
+#     consile.log("this is equal")
+# }else{
+#     print("this is not equal")
+# }
+
+# if 10 == 10:
+#     print("helo")
+    # sldbsld
+    # asdjasljd
+    
+
+
+# comments: to increse code readability.
+
+# 1) single line cmment
+
+# ljasdaskbdas dkasbdjas dasd jsa
+
+
+# 2) multiline comment
+
+"""
+akjbsdkjasd
+askdjbaskjdbasd
+asdjbasdsadbksakjdsad
+asdkjsabdjsadb
+"""
+
+
+
+# document interlude:
+
+
+# def car():
+#     """
+#     this is my fucation:
+#         1) this is a good guakj
+#         2) abc
+#         this is ok
+#         this fucantuon ony print helo
+#         thak you"""
+#     print("helooo")
+
+# # car()
+
+# help(input)
+
+
+# Core funcations:
+
+# 1) print() : use to print anything on console
+
+# 2) input() : to get input from user
+# 3) type()
+# 4) len()
+# 5) count()
+
+# x = 100
+
+# print("this is ok", x)
+
+# f-string
+
+
+# print(f"this is {x} value of {x} nooooooo {x}")
+
+
+
+# Type casting: cahnge type of a variable
+
+# 3) type() : helps you to know about what type of data a varible hold
+
+# a = {10, 29, 90, 78, 10, 100}
+
+# a = {"name" : "kriss", "age" : 20}
+
+# print(type(a))
+
+# a = 10
+
+# print(a, type(a))
+
+# b = float(a)
+# print(b, type(b))
+
+
+
+
+
+# a = "200"
+# print(a, type(a))
+
+# b = int(a)
+# print(b , type(b))
+
+# int()
+# float()
+# str()
+# list()
+# tuple()
+# set()
+
+# a = -293129
+# print(a, type(a))
+
+# b = bool(a)
+# print(b, type(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
