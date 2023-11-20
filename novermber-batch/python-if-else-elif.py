6db7230d6726cedde766904f1f18049cd1cf5462
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-19 23:15:17 -0800

    Create python-if-else-elif.py

---
+++ novermber-batch/python-if-else-elif.py
@@ -0,0 +1,99 @@
+# CONTROL STATEMENTS : those statments which control the flow of execution of a program
+
+# Condational statments : 
+# if else elif
+
+
+# comparsion operators
+
+# ==
+# <=
+# >=
+# >
+# <
+# !=
+
+
+# print(10 < 11)
+
+
+# if condation:
+#     sk dsdj fajsd
+#     asdjkajkd asd
+
+# ksjbdasbdkasd
+
+
+# a = 7
+
+#     # 5 < 10
+# if a < 10:
+#     print("these e ok")
+#     print("asljdbakjdbksads")
+#     print("asdkjabsdjksabdksajd")
+
+
+# elif conadation:
+#     sssdfdfds
+
+
+# else:
+#     print("heloooooooooooooooooo")
+
+
+        # Voating system
+    
+# age = int(input("Enter your age : "))
+
+
+# if age > 18:
+#     if age == 17:
+#         print("nooooooo")
+    
+#     elif age == 90:
+#         print("hlo")
+#     else:
+#         print("you are eligible for voat")
+    
+# elif age == 18:
+#     print("youor first voat")
+
+# elif age == 19:
+#     print("this is your second voat")
+
+
+# else:
+#     print("you are not eligible for voat")
+
+
+
+ **********calculater********
+ 
+ num1 = ?
+ num2 = ?
+ 
+ " Wecone to my calculater"
+ 1) Adation
+ 2) Substration
+ 3) multiplication
+ 4) devsion
+ 
+ choce = "enter what you want to do : "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
