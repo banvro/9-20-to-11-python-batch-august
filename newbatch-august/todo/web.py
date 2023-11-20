d8bd119f86232c17785a618cb826fcb74610fa59
Naresh Kumar <banvro07@gmail.com>
2023-11-20 01:50:26 -0800

    d

--- newbatch-august/todo/web.py
+++ newbatch-august/todo/web.py
@@ -45,11 +45,44 @@ def showdata():
 @web.route("/deletedata/<int:x>", methods=["POST"])
 def delete(x):
     zx = TodoSaveData.query.filter(TodoSaveData.id == x).first()
-    zx.title = "this is update title"
-    # db.session.delete(zx)
+    # zx.title = "this is update title"
+    db.session.delete(zx)
     db.session.commit()
 
     return redirect("/showdata")
 
+
+@web.route("/update/<int:x>",methods=["POST"])
+def update(x):
+    data = TodoSaveData.query.all()
+
+    mytodo = TodoSaveData.query.filter(TodoSaveData.id == x).first()
+
+
+    return render_template("basic_files/updatedata.html", alldata= data, x = mytodo)
+
+
+@web.route("/updatethis/<int:x>", methods=["POST"])
+def updatdtaajsdb(x):
+    if request.method == "POST":
+        Title = request.form.get("title")
+        Description = request.form.get("description")
+            
+        zx = TodoSaveData.query.filter(TodoSaveData.id == x).first()
+        zx.title = Title
+        zx.descrption = Description
+        
+        db.session.commit()
+
+    return redirect("/showdata")
+
+
+
 if __name__=="__main__":
     web.run(debug=True)
+
+
+
+
+
+# from abc import ABC, abstractmethod
