7b102161438a4d52941ff640ed57defe12dc372b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-20 22:17:30 -0800

    Create 3rd-class-for-loop.py

---
+++ novermber-batch/3rd-class-for-loop.py
@@ -0,0 +1,71 @@
+# Loops : ittration/ repetation
+
+
+# 1) for Loop
+# 2) while Loop
+
+
+# ittrable objects : str, list, tuple, set, dictionry.
+
+
+# a = "this is a car"
+
+# for varibale in sequnce:
+    
+# for y in a:
+#     print(y)
+#     print("hellooooooooooooooooo")
+#     print("ok", y, y, "asdkbasdkbasdada")
+
+
+# range(start, end, increment)
+
+# start = 0
+# end = n - 1
+# increment = 1
+
+
+# for e in range(1, 4):
+#     print(e)
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# .
+# .
+# .
+# .
+# 2 x 10 = 20
+
+
+
+# 4 x 11 = 4
+# 4 x 12 = 8
+# 4 x 13 = 12
+# .
+# .
+# .
+# 4 x 20 = 40
+
+
+1
+22
+3
+4444
+5
+666666
+7
+88888888
+9
+10101010101010101010
+
+
+
+
+
+
+
+
+
+
