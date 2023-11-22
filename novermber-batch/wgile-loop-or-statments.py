cea7a3f2c0c07bc52162f50754736b44c265e977
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-21 20:44:42 -0800

    Create wgile-loop-or-statments.py

---
+++ novermber-batch/wgile-loop-or-statments.py
@@ -0,0 +1,144 @@
+# While Loop : 
+
+
+
+# while candation:
+#     # block of code
+
+# for i in range(1, 11):
+#     print(i)
+
+# a = 1
+#     # 2 < 11
+# while a < 11:
+#     print(a)
+#     a = a + 1
+
+
+
+
+# while True:
+#     chociw = input("Enter s for start and    q for quit : ")
+    
+    
+    
+# break
+# continue
+# pass
+
+
+# a = "this is a car"
+    
+
+# for i in a:
+#     print(i)
+#     print("heloooo")
+    
+
+# range(start, end, increment)
+
+
+
+# for jn in range(1, 20):
+#     print(jn)
+
+# a = [1, 12, 34, 23, 34, 23, 10, 89, 10]
+
+# for u in a:
+#     print(u)
+#     if u == 34:
+#         break
+    
+
+
+# for i in range(1, 20):
+#     if i == 15:
+#         break
+#     print(i)
+
+
+# for i in range(1, 20):
+#     if i % 2 != 0:
+#         break
+#     print(i)
+
+
+
+# for i in range(1, 20):
+#     if i == 9:
+#         continue
+#     print(i)
+    
+    
+
+
+# for i in range(1, 20):
+#     if i % 2 != 0:
+#         continue
+#     print(i)
+    
+
+# for i in range(1, 10):
+#     print("hlo")
+    
+# print("heloo")  
+    
+
+
+
+
+
+# a = 1
+
+# while a < 12:
+#     print(a)
+#     a = a + 1
+#     if a == 5:
+#         break
+
+
+# while True:
+#     choice = input("Enter s for start and q for quit : ")
+    
+#     if choice == "q":
+#         break
+    
+#     elif choice == "s":
+#         print("game stared .........")
+    
+#     else:
+#         print("ENter something valid")
+    
+    
+    
+# assert 11 == 10, "not equal"
+    
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+.
+.
+.
+.
+
+2 x 10 = 20
+
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
