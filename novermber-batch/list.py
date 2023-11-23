a28cba071824d4871f494f668509678b89ed2709
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-22 23:08:09 -0800

    Create list.py

---
+++ novermber-batch/list.py
@@ -0,0 +1,113 @@
+# data types:
+#     list
+#     tuple
+#     set
+#     dictionry
+
+
+# a = 10
+
+# b = "hlooo"
+
+# list : []
+
+# ordered, allow duplicate values, mutable
+
+
+# lst = [10, 20, 80, 89, 67, 10, 12, 12, "hlo", True, 12, 100]
+
+
+# print(lst)
+
+# print(type(lst))
+
+# indexing : 
+
+# lst = [10, 20, 80, 89, 67, 10, 12, 12, "hlo", True, 12, 100]
+
+# print(lst[1] , lst[2])
+# []
+
+
+
+# print(lst[8])
+
+# print(lst[-1])
+
+# slicing : 
+
+# [start : end : increment]
+
+# lst = [10, 20, 80, 89, 67, 10, 12, 12, "hlo", True, 12, 100]
+
+# # print(lst[2 : 6 : 2])
+
+# print(lst[ : 5])
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+
+# lst = [10, 20, 80, 89, 67, 10, 12, 12, "hlo", True, 12, 100]
+
+# print(lst[: : -1])
+
+
+# Mutable : (modify, chnage)
+
+
+# a = [1, 2, 3, "hlo", "car"]
+
+
+# to addd nw data
+# 1) append()
+# 2) insert()
+
+# a.append(1000)
+
+# a.insert(2, 4000)
+
+# print(a)
+
+
+
+
+# a = [1, 2, 3, "hlo", "car"]
+
+# to dlete data
+# 1) pop()
+# 2) remove()
+
+# a.pop()
+
+# a.remove(3)
+
+# print(a)
+
+
+
+# lst = [10, 20, 80, 89, 67, 10, 12, 12, "hlo", True, 12, 100]
+
+
+# # lst.remove(lst[-2])
+# # lst.remove(lst[-2])
+# # lst.remove(lst[-2])
+# lst.clear()
+# print(lst)
+
+
+
+# lst = []
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# 1 - ----- 50
+
+
+# a = []
+
+# b = []
+
+
+
