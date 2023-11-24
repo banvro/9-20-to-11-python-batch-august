93f58e768f26f7401d5c72ae2d62e2157a3299b8
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-23 20:37:20 -0800

    Create tuple.py

---
+++ novermber-batch/tuple.py
@@ -0,0 +1,43 @@
+# 1) List : ordered, allow duplicate values, mutable.
+
+# 2) Tuple : ordered, allow duplicate values, imutable
+# ()
+
+# tpl = (12, 100, "hlo", True, 10, 12)
+
+# print(tpl[6 : 2 : -1])
+
+# print(type(tpl))
+
+
+
+# ab = (12, 10, 100, 78)
+
+# lst = list(ab)
+
+# print(lst)
+
+# lst.append(1000)
+
+# print(lst)
+
+# tpl = tuple(lst)
+
+
+# print(tpl)
+
+
+a = [23, 34, 12, 78, 100, 90]
+
+b = ("this", "is", "a", "car", "bus", "train")
+
+
+
+["this", 23, "is", 34, "a", 12, "car", 78, "bus", 100, "train", 90]
+
+
+
+
+
+
+
