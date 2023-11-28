324506412d6f7a31f92a3bd8892a68785d3401f3
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-11-27 20:37:40 -0800

    Create set-dictioney-lectue.py

---
+++ novermber-batch/set-dictioney-lectue.py
@@ -0,0 +1,149 @@
+# list : ordered, allow duplicte values, mutable
+# tuple : ordered, allow duplicate values, imutable
+
+# set : unordered, do not allow duplicate values, mutable
+
+# {}
+
+# st ={12, 'hlo', 10.9, 100, 12, 100, "car"}
+
+# to add new data:
+
+# add()
+
+# st.add(1000)
+
+
+
+# print(st)
+# print(type(st))
+
+
+
+# to delete elemnet:
+
+# st ={12, 'hlo', 10.9, 100, 12, 100, "car"}
+
+# 1) remove()
+# 2) pop()
+
+# st.(pop)
+
+# st.remove(12)
+
+# print(st)
+
+
+
+# list : ordered, allow duplicte values, mutable
+# tuple : ordered, allow duplicate values, imutable
+
+# set : unordered, do not allow duplicate values, mutable
+
+# dictionry : ordered, do not allow duplicate values, mutable
+
+# {key : value, key : values, key : values}
+
+
+
+# JSON
+
+
+# di = {
+#     "name" : "moris", 
+#     "age" : 20, 
+#     "number" : 926493846
+    
+# }
+
+# print(di)
+
+# print(type(di))
+
+
+
+
+
+# di = {
+#     "name" : "moris", 
+#     "age" : 20, 
+#     "number" : 926493846,
+#     "age" : 89
+# }
+
+# print(di["name"])
+
+
+# print(di.keys())
+
+# print(di.values())
+
+# print(di)
+
+
+# a = 10
+
+# a = 100
+
+
+# print(a)
+
+
+di = {
+    "name" : "moris", 
+    "age" : 20, 
+    "number" : 926493846,
+    "age" : 90
+}
+
+
+# di["address"] = "this is address"
+
+# di["state"] = "Mohali"
+
+
+# 1) to delete
+# pop()
+
+di.pop("age")
+
+print(di)
+
+
+
+
+
+
+n = 16
+
+16
+11
+6
+1
+-4
+1
+6
+11
+16
+
+
+n = 15
+
+15
+10
+5
+0
+5
+10
+15
+
+
+
+
+
+
+
+
+
+
+
