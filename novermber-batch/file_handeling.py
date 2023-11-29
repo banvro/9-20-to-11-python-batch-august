f948911664a160ce8608ecd1e8604428aa9edfb2
Naresh Kumar <banvro07@gmail.com>
2023-11-28 20:38:30 -0800

    file handeling

---
+++ novermber-batch/file_handeling.py
@@ -0,0 +1,39 @@
+
+
+# fl = open("mytxt.txt", "r")
+
+# zx = fl.read()
+# print(fl.readline())
+# print(fl.readline())
+
+# print(zx)
+
+
+# writing in text file 
+
+
+# fl = open("hlo.txt", "w")
+
+# fl.write("aksjdbksabd")
+
+# fl.close()
+
+
+
+# append data in file 
+
+# fil = open("hlo23.txt", "a")
+
+# fil.write(" \n this data come from append mode")
+
+# fil.close()
+
+
+# num1 = ?
+# a.txt 
+
+# num2 = ?
+# b.txt 
+
+# zx = a.txt + b.txt
+# result.txt
