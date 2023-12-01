59bdff1cdb0e00c3bfd09ddd258166d692ce1637
Naresh Kumar <banvro07@gmail.com>
2023-11-30 21:57:15 -0800

    sve new data

--- newbatch-august/nov django/Main/blog/views.py
+++ newbatch-august/nov django/Main/blog/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from blog.models import ContactUs
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("this is home page")
+    # return HttpResponse("this is home page")
+    return render(request, "home.html")
 
 
 def aboutus(request):
     return HttpResponse("this is about us page")
 
 def contactus(request):
-    return HttpResponse("this is about us page")
+    if request.method == "POST":
+        nm = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("number")
+        message = request.POST.get("msg")
+
+        data = ContactUs(Name = nm, Email = email, Phone_Number = phone, Message = message)
+        data.save()
+
+
+    return render(request, "contact-us.html")
 
 def services(request):
     return HttpResponse("this is about us page")
