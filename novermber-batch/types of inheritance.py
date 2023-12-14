2319effcda1c756c60ed0ae44ed49c6ae9b1e036
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-12-13 20:56:42 -0800

    Create types of inheritance.py

---
+++ novermber-batch/types of inheritance.py
@@ -0,0 +1,76 @@
+# Inheritance : 
+
+# class cls1:
+#     def mthd1(self):
+#         print("tjhis is a metohd 1")
+    
+#     def mthd2(self):
+#         print("this is metho 2")
+
+
+# class cls2(cls1):
+#     def mthd3(self):
+#         print("thissi mehtod 3")
+  
+# ob = cls1() 
+# ob.mthd1()
+# ob.mthd3()
+# obj1 = cls2()
+
+# obj1.mthd3()
+
+# obj1.mthd2()
+
+
+# 1) Single level inheritance : 
+#     when 1 child have 1 parent
+
+
+# 2) Multiple inheritance:
+    # when 1 child have multiple parent
+
+# class cls1:
+#     def mthd1(self):
+#         print("this is method 1")
+    
+#     def mthd2(self):
+#         print("this is method 2")
+
+# class cls2:
+#     def mthd3(self):
+#         print("this jsi method 3")
+    
+# class cls3(cls1, cls2):
+#     def mthd4(self):
+#         print("this si metho 4")
+
+# ob = cls3()
+# ob.mthd1()
+
+
+
+# 3) Multilevel Inheritance:
+#     garntparent <- parent <- child
+
+# class cls1:
+#     def mthd1(self):
+#         print("this si mehtod 1")
+    
+#     def mthd2(self):
+#         print("this is method 2")
+
+# class cls2(cls1):
+#     def mthd3(self):
+#         print("this is mehtod 3")
+
+# class cls3(cls2):
+#     def mthd4(self):
+#         print("thia is method 4")
+
+
+# ob = cls3()
+# ob.mthd1()
+
+
+
+
