ed817029914a8a387559c42e8cdc5fa98ff0e98c
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-12-25 20:32:39 -0800

    Create method-overloading.py

---
+++ novermber-batch/method-overloading.py
@@ -0,0 +1,108 @@
+# Polymopshism:
+#     1) Compiletime polymorphisn
+#         1) Operator Ovelaoding
+#         2) Method Overloading
+    
+#     2) RUn tim epolymorphism
+#         1) Metod OVERRIDING
+
+
+# Method Overloading : 
+
+
+# class cls1:
+#     def sumthis(self, a, b, c):
+#         zx = a + b +c
+#         print(zx)
+        
+#     def sumthis(self, a, b):
+#         zx = a + b
+#         print(zx)
+        
+#     def sumthis(self, a):
+#         zx = a 
+#         print(zx)
+
+# obj = cls1()
+# obj.sumthis(10, 20)
+
+
+# # a = 10
+
+# a = 100
+
+
+# class cls1:
+#     def sumthis(self, a = None, b = None, c = None):
+#         if a != None and b != None and c != None:
+#             zx = a + b +c
+#             print(zx)
+        
+#         elif a != None and b != None :
+#             zx = a + b 
+#             print(zx)
+        
+#         elif a != None:
+#             zx = a
+#             print(zx)
+
+# obj = cls1()
+# obj.sumthis(10, 20, 30)
+
+
+
+
+# Errors In python:
+#     1) Compile Time Error (Syntex Error)
+#     2) Logical Error
+#     3) Run time Error
+
+
+# a = 10
+# b  20
+# c = a + b
+# print(c)
+
+
+# if 10 == 10
+#     print("thesea are euqal")
+
+# 2) Logical Error 
+
+# a = 10
+# b = 0
+
+# c = a / b
+
+# print(c)
+
+
+
+# a = 10
+
+# b = "hey"
+
+# c = a + b
+# print(c)
+
+
+# 3) RUn time errors : 
+
+
+# age = int(input("Enter Your Age : "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
