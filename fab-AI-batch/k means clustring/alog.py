afcb181f24e377c4e222638c9a177f749f19f9a2
Naresh <banvro07@gmail.com>
2024-04-24 11:18:58 +0530

    k-means

--- fab-AI-batch/k means clustring/alog.py
+++ fab-AI-batch/k means clustring/alog.py
@@ -8,17 +8,34 @@ class MyKMeanssAlgo:
         self.centriod = None
     
     def fit_predict(self, x):
-        # print(x.shape[0])
-        # random(range, number of values)
-        # print(random.sample(range(0, x.shape[0]), self.n_cluster))
-        centriod_index = random.sample(range(0, x.shape[0]), self.n_cluster)
+        # 1) decide n cluster
+        # 2) init centriod 
+        # print(x)
+        for i in range(500):
+            centriod_index = random.sample(range(0, x.shape[0]), self.n_cluster)
 
-        self.centriod = x[centriod_index]
-        # print(self.centriod)
+            self.centriod = x[centriod_index]
+            
+            # 3) assign cluster 
+            cluster_group = self.calculate_distance(x)
 
-        cluster_group = self.calculate_distance(x)
+            # 4) move centriod 
 
-        print(cluster_group)
+            old_centriod = self.centriod
+
+            self.centriod = self.move_centriod(x, cluster_group)
+
+            print("goooooo")
+            print(type(self.centriod))
+            print(type(old_centriod))
+            # 5) finish
+            if (old_centriod == self.centriod).all():
+                print("heyyyyyy")
+                break
+        
+            return cluster_group
+
+        # print(cluster_group)
     
     def calculate_distance(self, x):
         distance = []
@@ -33,4 +50,14 @@ class MyKMeanssAlgo:
             cluster_group.append(distance.index(min(distance)))
             distance.clear()
 
-        return cluster_group
+        return np.array(cluster_group)
+
+
+    def move_centriod(self, x, cluster_group):
+        new_centriod = []
+        my_clusters = np.unique(cluster_group)
+
+        for cluster in my_clusters:
+            new_centriod.append(x[cluster_group == cluster].mean(axis = 0))
+
+        return np.array(new_centriod)
--- fab-AI-batch/k means clustring/app.py
+++ fab-AI-batch/k means clustring/app.py
@@ -6,12 +6,21 @@ centriod = [(-5, -5), (5, 5)]
 
 centriod_sdd = (1,1)
 
-x, y = make_blobs(n_samples = 100, centers = centriod, cluster_std = centriod_sdd, n_features = 2)
+x, y = make_blobs(n_samples = 500, centers = centriod, cluster_std = centriod_sdd, n_features = 2)
 
 # print(x)
 # plt.scatter(x[:, 0], x[:, 1])
 # plt.show()
 
-mymodel = MyKMeanssAlgo(n_cluster = 2, max_iter = 100)
+mymodel = MyKMeanssAlgo(n_cluster = 2, max_iter = 1000)
 
-mymodel.fit_predict(x)
+y_pred = mymodel.fit_predict(x)
+
+
+# print(y_pred)
+
+plt.scatter(x[y_pred == 0, 0], x[y_pred == 0, 1], color = "red")
+plt.scatter(x[y_pred == 1, 0], x[y_pred == 1, 1], color = "blue")
+# plt.scatter(x[y_pred == 2, 0], x[y_pred == 2, 1], color = "green")
+
+plt.show()
