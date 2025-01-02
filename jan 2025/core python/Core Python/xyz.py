4da55232daf4504e3a116f6513f8b46608b2abb3
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-01-02 10:48:55 +0530

    Add files via upload

---
+++ jan 2025/core python/Core Python/xyz.py
@@ -0,0 +1,68 @@
+    # print("hellow world")
+
+    # 700+ -----> python
+
+
+    Python : ---> 
+
+            History:  1980
+
+        Gudo van Rossum
+
+
+    1) ABC language ()
+    2) C 
+    3) Unix
+
+
+    #include <stdio.h>
+    #include <conio.h>
+
+    int main(){
+            int a;
+            int b;
+            int c;
+
+            a = 10;
+            b = 20;
+            c = a + b;
+            printf("%d", c);
+
+            return 0;
+    }       
+
+
+    1980 --------- 1991 
+            ------------------> Python 0.9.0 
+
+
+    2000 -----> python 2   -----------> 2020
+
+    2008 ------> python 3
+
+    ---> python 3.13.0
+
+
+    Why Python:
+        1) Simple 
+        2) Gernal Puropse
+        3) Heigh level
+        4) Object Oriented
+        5) Interpreated
+        6) Scripting
+        7) Dnynmically Typed
+        8) Case Senstive
+
+
+
+    Python -------->
+        MySQL
+        Projects
+
+
+
+    8219836118
+
+
+
+
