be95ee5871bf4e10054dc48fbe51dc50e4ec407b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2025-01-03 11:10:40 +0530

    Add files via upload

---
+++ jan 2025/core python/Core Python/learn_veriables.py
@@ -0,0 +1,150 @@
+# Python Veriables:
+    
+# Rules:
+    # 1) Veriable always start from charcter or underscore.
+    # 2) Veribales never be start from integers.
+    # 3) Veriables never holds spaces or spacial symbols.
+    # 4) Veriables never be a keyword.
+    
+# full_name = "helloooooo"   
+    
+    
+    # @#!#$%^&*()_)(*&^%^&*()_{})
+    
+    
+    
+# x = "hloooo"
+    
+    
+    
+    
+    
+# 1hasdaksdka = 207490234    
+    
+# a1 = 2934
+    
+    
+    
+    
+    
+# a = 10
+# akbjdaksbdk = 92
+
+# AkbsakdjbKGIT = 297402
+
+# _andjk = 10
+
+
+
+# 4) Veriables never be a keyword.
+
+
+# Keywords : are those words which are already reserved by Python for any spacial purpose.
+
+# int, float, str, bool, def, class
+
+
+# int = 10
+
+# class = 20
+
+
+# full_name = "helloo"
+
+
+
+# Core functions:
+    # 1) Built in functions
+    # 2) User define functions
+
+
+# 1) print()
+# 2) input()
+# 3) count()
+# 4) len()
+# 5) type()
+
+
+
+# Data Types :    
+#     1) Integer --> 0, 12, -234324, 43553
+    
+#     2) String ---> 
+#         'this si a string'
+#         "this is also a string"
+#         """this is also a string"""
+        
+#         "10"
+    
+#     3) Float ---> 10.0, 34.456, 
+    
+#     4) Boolean ---> True, False
+    
+#     5) Complex --> a + bj
+#                 10 + 20j
+                
+        # 6) List
+        # 7) Set
+        # 8) Tuple
+        # 9) Dictionry
+
+
+# print() --> helps you to print anyting on console/terminal.
+
+
+# print("this is a car")
+
+# print('this is a carrrrr')
+
+# print("""this is adasdsadad""")
+
+
+# print("""ehllo,
+        # how are you
+        # asdsad
+        # a
+        # sd
+        # asd
+        # a""")
+
+
+# a = 100
+# b = 200
+# c = "this is a car"
+# print(a, b, c, "eeeeeeeeeee")
+
+
+
+# a = 10
+# b = 20
+
+# # c = a + b
+
+# # print(c)
+
+# print(a + b)
+
+
+# type() ---> it returns which type of data a veriable hold..
+
+
+# a = 12 + 78j
+
+# print(type(a))
+
+
+# x = "True"
+
+# print(type(x))
+
+
+
+# Python Propertiees
+# Veriables
+# Data Types
+# keywords
+# print() and type() 
+
+
+
+
